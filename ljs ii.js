99dd68847c2c75e4d89687edc19b018b1ef83b4c
Arivanandan <uarivanandan@gmail.com>
2016-10-17 15:16:22 +0530

    Update ljs ii.js

--- ljs ii.js
+++ ljs ii.js
@@ -20,13 +20,24 @@ function identifierParser (input) {
   return [node, input.replace(word, '')]
 }
 
+function stringParser (input) {
+  if (input.charAt(0) !== '"') return null
+  var str = input.substring(1, input.lastIndexOf('"'))
+  var node = { type: 'StringLiteral', value: str }
+  str = '"' + str + '"'
+  return [node, input.replace(str, '')]
+}
+
 function keywordParser (input) {
   var kwRegEx = (/^\w+|^[*+-<>%\/]/)
   var word = kwRegEx.exec(input)
   if (!word || !~keywords.indexOf(word[0])) return null
   if (word[0] === 'define') return declaratorParser(input)
   if (word[0] === 'lambda') return lambdaParser(input)
-  if (!!~operators.indexOf(word[0])) return operatorParser(input)
+  if (word[0] === 'if') return ifParser(input)
+  if (!!~arithmeticOperators.indexOf(word[0])) return arithmeticParser(input)
+  if (!!~binaryOperations.indexOf(word[0])) return binaryParser(input)
+  if (!!~unaryOperations.indexOf(word[0])) return unaryParser(input)
 }
 
 function declaratorParser (input) {
@@ -45,7 +56,7 @@ function lambdaParser (input) {
   return [node, expr[1]]
 }
 
-function operatorParser (input) {
+function arithmeticParser (input) {
   var operator = input.charAt(0)
   input = input.replace(operator, '')
   var expr = seParser(input)
@@ -54,18 +65,35 @@ function operatorParser (input) {
   return [node, expr[1]]
 }
 
+function binaryParser (input) {
+  var operator = input.charAt(0)
+  input = input.replace(operator, '')
+  var expr = seParser(input)
+  if (!expr) return null
+  var node = { 'type': 'BinaryExpression', 'operator': operator, 'left': expr[0][0], 'right': expr[0][1] }
+  return [node, expr[1]]
+}
+
+function unaryParser (input) {
+  var operator = (/^\w+/).exec(input).toString()
+  input = input.replace(operator, '')
+  var expr = seParser(input)
+  if (!expr) return null
+  var node = { 'type': 'Expression', 'operator': operator, 'arg': expr[0] }
+  return [node, expr[1]]
+}
+
+function ifParser (input) {
+  input = input.replace('if', '')
+  var expr = seParser(input)
+  var node = { 'type':'IfStatement', 'test':expr[0][0][0] , ',consequent': expr[0][0][1][0], 'alternate': expr[0][0][1][1] }
+  return [node, expr[1]]
+}
+
 function atomParser (input) {
-  var preParse
-  preParse = spaceParser(input)
-  if (preParse) input = preParse[1]
-  preParse = keywordParser(input)
-  if (preParse) return preParse
-  preParse = identifierParser(input)
-  if (preParse) return preParse
-  preParse = numberParser(input)
-  if (preParse) return preParse
-  preParse = expressionParser(input)
-  if (preParse) return preParse
+  var selectedParser = parserSelector([keywordParser, identifierParser, numberParser, stringParser, expressionParser])
+  var out = selectedParser(input)
+  if (out) return out
   return null
 }
 
@@ -74,11 +102,13 @@ function expressionParser (input) {
   var arr = []
   var expr = slicer(input)
   input = expr[0]
-  out = seParser(input)
-  arr.push(out[0])
+  var out = seParser(input)
+  var ast = out[0]
+  for (nodes in ast) arr.push(ast[nodes])
   if (expr[1]) {
     input = expr[1]
-    arr.push(seParser(input)[0].pop())
+    var ast = seParser(input)[0]
+    for (nodes in ast) arr.push(ast[nodes])
   }
   return [arr, out[1]]
 }
@@ -94,6 +124,17 @@ function seParser (input) {
   return [arr, input]
 }
 
+function parserSelector (parsers) {
+  return function (string) {
+    if (spaceParser(string)) string = spaceParser(string)[1]
+    for (var parser of parsers) {
+      var preParse = parser(string)
+      if (preParse) return preParse
+    }
+    return null
+  }
+}
+
 function slicer (input) {
   var openingParen = 1
   var i = 1
@@ -106,8 +147,11 @@ function slicer (input) {
   return input.slice(1, i) + ' ' + input.substr(i + 1)
 }
 
-var keywords = ['define', 'lambda', '*', '+', '-', '/', '<', '>', '%']
-var operators = ['*', '+', '-', '/', '<', '>', '%']
+var keywords = ['define', 'lambda', '*', '+', '-', '/', '<', '>', '<=', '>=', '%', 'if',
+'length', 'abs', 'append', 'pow', 'min', 'max', 'round', 'not', 'quote']
+var binaryOperations = ['<', '>', '<=', '>=', 'pow', 'append']
+var unaryOperations = ['length', 'abs', 'round', 'not']
+var arithmeticOperators = ['*', '+', '-', '/', '%']
 
 const readline = require('readline')
 const rl =  readline.createInterface({
