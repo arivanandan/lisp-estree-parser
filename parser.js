08fceefce71cecc2f4b48cf7f56e88f050884cf3
Arivanandan <uarivanandan@gmail.com>
2016-10-31 11:01:22 +0530

    works for all basic functions

--- parser.js
+++ parser.js
@@ -1,9 +1,9 @@
 module.exports = function (input) {
   const unaryOperations = ['length', 'abs', 'round', 'not', 'min', 'max', 'round', 'quote']
   const arithmeticOperators = ['*', '+', '-', '/', '%', '<', '>', '<=', '>=', 'pow', 'append']
-  const variables = []
-  const body = []
-  const astBody = []
+  const variables = []                                              //stores identifiers in interpreted program
+  const body = []                                                   //stores body of single expression statement
+  const astBody = []                                                //stores generated ast of entire program
 
   function spaceParser (input) {
     return (/^\s+/).test(input) ? [null, input.replace(/\s+/, '')] : null
@@ -63,9 +63,11 @@ module.exports = function (input) {
 
   function binaryParser (input, word) {
     const node = { type: 'BinaryExpression', operator: word, left: input[0], right: input[1] }
+    node.left = input[0][0] ? input[0][0] : input[0]
+    node.right = input[1][0] ? input[1][0] : input[1]
     return node
   }
-
+//reduce function for operations with more than 2 arguments
   function reduceArr (input, word) {
     const node = { type: 'ExpressionStatement',
       expression: { type: 'CallExpression',
@@ -116,7 +118,7 @@ module.exports = function (input) {
     if (!expr) return null
     const node = { type: 'ExpressionStatement', expression: {
       type: 'CallExpression', callee: { type: 'Identifier', name: word },
-      arguments: expr[0] || '' }}
+      arguments: expr[0].pop() || '' }}
     return [node, expr[1]]
   }
 
@@ -145,7 +147,7 @@ module.exports = function (input) {
     node.expression.alternate = expr[0][2][0] ? expr[0][2][0] : expr[0][2]
     return [node, expr[1]]
   }
-
+//loops through until all the tokens in an expression are consumed
   function expressionParser (input) {
     let arr = []
     while (input.length > 0) {
@@ -161,7 +163,7 @@ module.exports = function (input) {
     }
     return [arr, input]
   }
-
+//interprets subexpressions (i.e expressions within brackets)
   function seParser (input) {
     if (!(/^\s*\(/).test(input)) return null
     if (spaceParser(input)) input = spaceParser(input)[1]
@@ -172,22 +174,23 @@ module.exports = function (input) {
       if (openingParen === 0) return [expressionParser(input.slice(1, i))[0], input.substr(i + 1)]
     }
   }
-
+//accepts list of parsers, returns appropriate parser
   const parserFactory = (...parsers) => function (input) {
     if (spaceParser(input)) input = spaceParser(input)[1]
     return parsers.reduce((acc, parser) => acc === null ? parser(input) : acc, null)
   }
-
+//tries the 4 main parsers; alternatively calls the expressionParser which tries the rest
   function sExpressionParser (input) {
     if (/^\s*\(*\s*const/.exec(input)) return declaratorParser(input)[0]
     if (/^\s*\(*\s*=>/.exec(input)) return lambdaIIFEParser(input)[0]
     if (/^\s*\(*\s*if/.exec(input)) return ifParser(input)[0]
+    if (functionCallParser(input)) return functionCallParser(input)[0]
     expressionParser(input)[0].forEach(function (atom) {
       body.push(atom)
     })
     return body
   }
-
+//splits the program with /newline and passes them one by one
   function programParser (input) {
     input = input.split('\n')
     for (let i = 0; i < input.length; i++) {
